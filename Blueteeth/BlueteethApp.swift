353dfe31267b50e4ae2be124cb01e76393bbc7cb
prathasaxena <prathasaxena30@gmail.com>
2023-11-02 20:34:17 +0530

    Initial Commit

---
+++ Blueteeth/BlueteethApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueteethApp.swift
+//  Blueteeth
+//
+//  Created by Pratha Saxena on 02/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BlueteethApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
